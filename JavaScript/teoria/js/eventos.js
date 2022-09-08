f80fbf47fc58332b9bc82e7402b9431246c13884
Arthur-Gab <arthurgabriel933@gmail.com>
2022-09-08 16:01:20 -0300

    Realizado alguns exercicios e Estudo sobre Events DOM

---
+++ JavaScript/teoria/js/eventos.js
@@ -0,0 +1,37 @@
+/*
+    Mouse Events
+    onclick - clique 1x
+    ondblclick - clique 2x
+    onmouseup - clique liberado
+    onmouseover - cursor em cima do elemento
+    onmouseout - cursor sai de cima do elemento
+*/
+
+function alertBtn() {
+    alert('Evento de alert');
+}
+
+function alertBtnClick() {
+    alert('Clicou no button');
+}
+
+function alertBtnOut() {
+    alert('Cursor saiu da área do button');
+}
+
+/*
+    Teclado Events
+    onkeydown - tecla pressionada
+    onkeypress - mantem a tecla pressionada (teclas com caracter)
+    onkeyup - tecla liberada
+*/
+
+function teclaAlert() {
+    alert('Evento de teclado disparado');
+}
+
+/*
+    Janela/Frame Events
+    onresize - capturar a janela e disparar uma ação dependendo da resolução
+    onscroll - quando o scroll do mouse é acionado
+*/
