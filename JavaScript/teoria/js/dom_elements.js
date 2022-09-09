64669d1ad260c6bcdad982cce88849991cbce260
Arthur-Gab <arthurgabriel933@gmail.com>
2022-09-09 15:27:57 -0300

    Aprendendo um pouco sobre manipulação de valores txt pelo DOM

---
+++ JavaScript/teoria/js/dom_elements.js
@@ -0,0 +1,19 @@
+function confirmDataType() {
+
+    // Selecionar o valor digitado
+    let caracter = document.getElementById('entrada').value
+
+    // Limpar o campo de digitação
+    document.getElementById('entrada').value = ''
+
+    //Trata a var pra valer so o 1° digito
+    caracter = caracter.charAt(0)
+
+    if (isNaN(parseInt(caracter))) {
+        document.getElementById('apenas_caracter').value = caracter
+    }
+    else {
+        document.getElementById('apenas_numero').value = caracter
+    }
+
+}
