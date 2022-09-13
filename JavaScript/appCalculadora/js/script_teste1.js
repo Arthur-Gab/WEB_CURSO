fd444b181ab36801f8d38626d5ea1ca208876e17
Arthur-Gab <arthurgabriel933@gmail.com>
2022-09-13 16:58:23 -0300

    Projeto Calculadora concluido, mas fiquei com uma pulga atras da orelha por nao ser capaz de usar uma variavel pra deixar o codigo mais limpo ao referenciar o #result

--- JavaScript/appCalculadora/js/script_teste1.js
+++ JavaScript/appCalculadora/js/script_teste1.js
@@ -1,5 +1,27 @@
 function calcular(tipo, valor) {
     // acao (c, /, *, -, +, =)
     // numero (0 a 9)
-    console.log(tipo, valor)
+    if (tipo === 'acao') {
+        // Action
+        if (valor === 'c') {
+            // Clear
+            document.querySelector('#resultado').value = ''
+        }
+        else if (valor === '/' || valor === '*' || valor === '-' || valor === '+' || valor === '.') {
+            // Divide, Multplication, Subtraction, Addition, Dot
+            document.querySelector('#resultado').value += valor
+            console.log(document.querySelector('#resultado').value)
+        }
+        else if (valor === '=') {
+            // Result
+            document.querySelector('#resultado').value = eval(document.querySelector('#resultado').value)
+            console.log(document.querySelector('#resultado').value)
+        }
+    }
+    else if (tipo === 'numero') {
+        // Number  
+        document.querySelector('#resultado').value += valor
+        console.log(document.querySelector('#resultado').value)
+    }
+
 } 
