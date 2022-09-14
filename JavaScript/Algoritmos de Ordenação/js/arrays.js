7b471ffa92b20a1638ed4a8489359336d6bd7a63
Arthur-Gab <arthurgabriel933@gmail.com>
2022-09-13 21:01:32 -0300

    Testando aprendizados da Faculdade - Algoritmos de Ordenação (Bubble Sort)

---
+++ JavaScript/Algoritmos de Ordenação/js/arrays.js
@@ -0,0 +1,38 @@
+/*  
+    Goals:
+    Criar Array, 
+    Ordenar o Array
+    Ser uma Interface pro usuario ver o que está acontecendo pelo console 
+*/
+
+// Imports
+import {bubbleSort} from './bubble_sort.js'
+
+
+// Functions
+function creatArray() {
+    // Cria um array e preenche ele com numeros randomicos de 0 a 99
+
+    // Captar o Tamanho do Array
+    const arraySize = prompt('Tamanho do Array: ');
+    console.log('Tamanhao: ' + arraySize);
+
+    // Criar um Array do tamanho que foi pedido
+    const array = new Array(parseInt(arraySize));
+
+    // Preencher esse array com numero randons
+    for (let i = 0; i < parseInt(arraySize); i++) {
+        array[i] = Math.floor(Math.random() * 100);
+    }
+    
+    // Retornar o array para ele ser usado em outras funções
+    return array;
+}
+
+// Program
+
+// 1° Criar o array (preenchico)
+// 2° Ordenar
+
+// Ordenar por Bubble Sort
+bubbleSort(creatArray());
---
+++ JavaScript/Algoritmos de Ordenação/js/bubble_sort.js
@@ -0,0 +1,24 @@
+// Ordenação Bubble Sort
+export function bubbleSort(array) {
+
+    // Mostrar o arry antes da ordenação
+    console.log(array);
+
+    let swap;
+    // Caso a lista ja esteja ordenada o algoritmo não precisará continuar a verificar se está ordenada 
+    // igual a um bubbleSort comum, até o termino de um for, como exemplo
+    do {
+
+        swap = false;
+        for (let i = 0; i < array.length - 1; i++) {
+
+            if (array[i] > array[i+1]) {
+
+                [array[i], array[i+1]] = [array[i+1], array[i]];
+                swap = true;
+            }
+
+            console.log(array);
+        }
+    } while (swap)
+}
