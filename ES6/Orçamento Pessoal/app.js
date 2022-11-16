2593a58c2ee278478557decca5589714ce2ae14c
Arthur-Gab <arthurgabriel933@gmail.com>
2022-11-16 20:52:14 -0300

    atualizanod algumas coisas

--- ES6/Orçamento Pessoal/app.js
+++ ES6/Orçamento Pessoal/app.js
@@ -17,7 +17,7 @@ class UserCreatOrFinderExpense {
     addEventClickerConfer() {
         // Put click events for Confer.html here
         this.finder.addEventListener("click",
-            this.finderExpense);
+            this.searchExpense);
     }
 
     registerExpense() {
@@ -54,9 +54,9 @@ class UserCreatOrFinderExpense {
 
     }
 
-    finderExpense() {
+    searchExpense() {
+
         // Recuperar o valor dos inputs e retornar eles na table como uma despesa
-        console.log("Finder Working");
         this.Expense = new Expense(
             "#day",
             "#month",
@@ -67,7 +67,7 @@ class UserCreatOrFinderExpense {
         );
 
         this.Storage = new Storage(this.Expense);
-        this.Storage.findExpense();
+        this.Storage.filterExpense();
     }
 
     init() {
@@ -151,6 +151,7 @@ class Storage {
         // For recording this.Expense is an objLiteral
         // For recording this.Expense is a class Expense
         this.Expense = Expense;
+        this.listOfExpenses = document.querySelector("#list-expenses");
     }
 
     recordExpense() {
@@ -159,25 +160,55 @@ class Storage {
         console.log(this.Expense);
     }
 
-    findExpense() {
+    filterExpense() {
         // Get the values of the inputs and search in local storage for an Expense compatible
         // it's necessary the input year is filled at least
-        const day = this.Expense.day.value;
-        const month = this.Expense.month.value;
-        const year = this.Expense.year.value;
-        const type = this.Expense.type.value;
-        const description = this.Expense.description.value;
-        const cost = this.Expense.cost.value;
+        // const day = this.Expense.day.value;
+        // const month = this.Expense.month.value;
+        // const year = this.Expense.year.value;
+        // const type = this.Expense.type.value;
+        // const description = this.Expense.description.value;
+        // const cost = this.Expense.cost.value;
+        let expenses = Array();
 
-        console.log(day)
+        for (let i = 1; expenses.length < localStorage.length; i++) {
 
-        for (let i = 1; i < localStorage.length + 1; i++) {
+            let expense = JSON.parse(localStorage.getItem(i));
 
-            let despesa = JSON.parse(localStorage.getItem(i));
+            if (expense === null)
+                continue;
 
-            console.log(i, despesa.day);
+            expenses.push(expense);
         }
 
+        // <tr>
+        //     <td id="table-date">12/03/2020</td>
+        //     <td id="table-type">Education</td>
+        //     <td id="table-description">Mensalidade Facu</td>
+        //     <td id="table-cost">1200</td>
+        //     <td id="td-closer">
+        //         <button id="tr-closer" class="btn closer">
+        //             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512">
+        //                 <!--! Font Awesome Pro 6.2.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2022 Fonticons, Inc. -->
+        //                 <path
+        //                     d="M310.6 150.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L160 210.7 54.6 105.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L114.7 256 9.4 361.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0L160 301.3 265.4 406.6c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L205.3 256 310.6 150.6z" />
+        //             </svg>
+        //         </button>
+        //     </td>
+        // </tr>
+
+        expenses.forEach((e) => {
+            
+            // Creating <tr>
+            let row = this.listOfExpenses.insertRow();
+
+            // Creating <td>
+            row.insertCell(0).innerHTML = `${e.day}/${e.month}/${e.year}`;
+            row.insertCell(1).innerHTML = `${e.type}`;
+            row.insertCell(2).innerHTML = `${e.description}`;
+            row.insertCell(3).innerHTML = `R$ ${e.cost}`;
+
+        })
     }
 
     getIndex() {
