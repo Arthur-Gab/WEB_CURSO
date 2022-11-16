c29b04b3827080cf0c282628f0a13bd97293e600
Arthur-Gab <arthurgabriel933@gmail.com>
2022-11-16 12:44:54 -0300

    Arrumado um detalhe de poscionamento do header e do menu-hamburguer, foi consertado a gravação da despesa, que agora é gravada como um obj Literal. Ademais, iniciei a programação da funcionalidade de recuperar a despesa e mostrar na tabela que deve aparecer somente quando for feito uma busca

--- ES6/Orçamento Pessoal/app.js
+++ ES6/Orçamento Pessoal/app.js
@@ -1,4 +1,4 @@
-import {Modal} from './modal.js';
+import { Modal } from './modal.js';
 
 class UserCreatOrFinderExpense {
     constructor(register, finder) {
@@ -36,8 +36,9 @@ class UserCreatOrFinderExpense {
         if (this.Expense.validate()) {
 
             // Valores validos - grava no Local Storage a Expense
-            this.Storage = new Storage(this.Expense);
+            this.Storage = new Storage(this.Expense.toObjLiteral());
             this.Storage.recordExpense();
+            this.Expense.clearInput();
         } else {
 
             // Valores Invalidos - Aparece um Modal centralizado
@@ -47,13 +48,26 @@ class UserCreatOrFinderExpense {
                 ".btn-txt-close"
             );
             MODAL.init();
-        }
 
+            this.Expense.error().focus();
+        }
 
     }
 
     finderExpense() {
+        // Recuperar o valor dos inputs e retornar eles na table como uma despesa
         console.log("Finder Working");
+        this.Expense = new Expense(
+            "#day",
+            "#month",
+            "#year",
+            "#type",
+            "#description",
+            "#cost"
+        );
+
+        this.Storage = new Storage(this.Expense);
+        this.Storage.findExpense();
     }
 
     init() {
@@ -66,30 +80,104 @@ class UserCreatOrFinderExpense {
 
 class Expense {
     constructor(day, month, year, type, description, cost) {
-        this.day = document.querySelector(day).value;
-        this.month = document.querySelector(month).value;
-        this.year = document.querySelector(year).value;
-        this.type = document.querySelector(type).value;
-        this.description = document.querySelector(description).value;
-        this.cost = document.querySelector(cost).value;
+        this.day = document.querySelector(day);
+        this.month = document.querySelector(month);
+        this.year = document.querySelector(year);
+        this.type = document.querySelector(type);
+        this.description = document.querySelector(description);
+        this.cost = document.querySelector(cost);
     }
 
     validate() {
-        if (this.day === "" || isNaN(this.day) || this.month === "" || this.year === "" || this.type === "" || this.description === "" || this.cost === "" || isNaN(this.cost))
+        if (this.day.value === "" || isNaN(this.day.value) || this.month.value === "" || this.year.value === "" || this.type.value === "" || this.description.value === "" || this.cost.value === "" || isNaN(this.cost.value))
             return false
 
         return true
+    }
+
+    error() {
+
+        // retornar o obj a ser focado
+        let error = null;
+        if (this.day.value === "" || isNaN(this.day.value))
+            error = this.day;
+        else if (this.month.value === "")
+            error = this.month;
+        else if (this.year.value === "")
+            error = this.year;
+        else if (this.type.value === "")
+            error = this.type;
+        else if (this.description.value === "")
+            error = this.description;
+        else if (this.cost.value === "" || isNaN(this.cost.value))
+            error = this.cost;
+
+        return error;
+    }
+
+    clearInput() {
+        this.day.value = "";
+        this.month.value = "";
+        this.year.value = "";
+        this.type.value = "";
+        this.description.value = "";
+        this.cost.value = "";
+    }
+
+    toObjLiteral() {
+        // Return the values of the Expense for recording in the localStorage
+        const day = this.day.value;
+        const month = this.month.value;
+        const year = this.year.value;
+        const type = this.type.value;
+        const description = this.description.value;
+        const cost = this.cost.value;
+
+        const pseudoExpense = {
+            day,
+            month,
+            year,
+            type,
+            description,
+            cost
+        }
+
+        return pseudoExpense;
     }
 }
 
 class Storage {
     constructor(Expense) {
+        // For recording this.Expense is an objLiteral
+        // For recording this.Expense is a class Expense
         this.Expense = Expense;
     }
 
     recordExpense() {
-        // Record Expenses in Local Storage;
+        // Record Expenses in Local Storage as a Obj Literal;
         localStorage.setItem(this.getIndex(), JSON.stringify(this.Expense));
+        console.log(this.Expense);
+    }
+
+    findExpense() {
+        // Get the values of the inputs and search in local storage for an Expense compatible
+        // it's necessary the input year is filled at least
+        const day = this.Expense.day.value;
+        const month = this.Expense.month.value;
+        const year = this.Expense.year.value;
+        const type = this.Expense.type.value;
+        const description = this.Expense.description.value;
+        const cost = this.Expense.cost.value;
+
+        console.log(day)
+
+        for (let i = 1; i < localStorage.length + 1; i++) {
+
+            let despesa = JSON.parse(localStorage.getItem(i));
+
+            console.log(i, despesa.day);
+        }
+
     }
 
     getIndex() {
